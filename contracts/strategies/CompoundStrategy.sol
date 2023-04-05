9a9787f1414c3944c7e92afc327518e58dc408bb
SmartOwl <smartowl02@outlook.com>
2023-04-05 16:00:03 +0200

    feat: add contract address

--- contracts/strategies/CompoundStrategy.sol
+++ contracts/strategies/CompoundStrategy.sol
@@ -28,13 +28,13 @@ contract CompoundStrategy is InitializableAbstractStrategy, CurveExchange {
     uint256 public storedPrimaryTokenBalance;
 
     IERC20 public token0;
-    IERC20 public primaryStable;
+    IERC20 public primaryStable; // 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174
 
-    address internal pTokenAddress;
+    address internal pTokenAddress; // 0x8505b9d2254A7Ae468c0E9dd10Ccea3A837aef5c
 
     address public oracleRouter;
-    address public cometAddress;
-    address public cometRewardAddress;
+    address public cometAddress; // 0xF25212E676D1F7F89Cd72fFEe66158f541246445
+    address public cometRewardAddress; // 0x45939657d1CA34A8FA39A924B71D28Fe8431e581
     address public curvePool;
 
     mapping(address => int128) internal curvePoolIndices;
